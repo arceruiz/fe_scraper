566fd6fc0266737420bbed55ae36c16ec7c79b45
Lucas Ruiz <arcehtm@gmail.com>
2020-10-22 22:50:59 -0300

    initial commit

---
+++ fe_scraper/scraper.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Html;
+using ScrapySharp.Html.Forms;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fe_scraper
+{
+    public static class scraper
+    {
+        public static void scrape()
+        {
+            ScrapingBrowser browser = new ScrapingBrowser();
+            WebPage homePage = browser.NavigateToPage(new Uri("http://www.bing.com/"));
+
+            PageWebForm form = homePage.FindFormById("sb_form");
+            form["q"] = "scrapysharp";
+            form.Method = HttpVerb.Get;
+            WebPage resultsPage = form.Submit();
+
+            HtmlNode[] resultsLinks = resultsPage.Html.CssSelect("div.sb_tlst h3 a").ToArray();
+
+            WebPage blogPage = resultsPage.FindLinks(By.Text("romcyber blog | Just another WordPress site")).Single().Click();
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,13 @@
+﻿using fe_scraper;
+using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            scraper.scrape();
+        }
+    }
+}
